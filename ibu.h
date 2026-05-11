b4fdabcd4ef3ab82eec0dbf7c37d02691eefc263
Khazz <thelenaid@gmail.com>
2026-05-11 17:58:28 +0700

    deklarasi member variabel pada class ibu

--- ibu.h
+++ ibu.h
@@ -5,5 +5,9 @@ class anak;
 
 //membuat class ibu pada file header ibu.h
 class Ibu {
+public:
+    //deklarasi member variabel pada class ibu
+    string nama;
+    vector<anak*> daftar_anak;
     
 }
